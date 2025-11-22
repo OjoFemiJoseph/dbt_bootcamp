140fa04b55eec5162c109278c5c8d72c1a2b27ce
Adegbite Ayoade Abel <tripleaceme@gmail.com>
2025-11-22 13:08:14 +0000

    updated schema name

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,13 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
+    {%- if custom_schema_name is none and target.name != 'prod' -%}
 
         {{ default_schema }}
 
     {%- else -%}
 
-        {{ default_schema }}_{{ custom_schema_name | trim }}
+        {{ custom_schema_name | trim }}
 
     {%- endif -%}
 
